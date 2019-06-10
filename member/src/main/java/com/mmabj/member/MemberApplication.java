3d969bdf1d9a6094058d7a0a9cc4174015cd33c1
Administrator <903296615@qq.com>
2019-06-10 11:12:24 +0800

    多份test/dev配置启动文件

--- member/src/main/java/com/mmabj/member/MemberApplication.java
+++ member/src/main/java/com/mmabj/member/MemberApplication.java
@@ -1,15 +1,10 @@
 package com.mmabj.member;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan(value = "com.mmabj.common")
+/*@ComponentScan(value = "com.mmabj.common")
 @MapperScan("com.mmabj.ctable.mapper")
-@SpringBootApplication
+@SpringBootApplication*/
 public class MemberApplication {
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class,args);
-    }
+    }*/
 }
